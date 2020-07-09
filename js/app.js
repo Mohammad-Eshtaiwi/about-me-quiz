5abc10c48bea079fe92162efa7f51a9ecd45fae4
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-07-09 14:36:40 +0300

    deleted every console.log

--- js/app.js
+++ js/app.js
@@ -1,45 +1,45 @@
-'use strict';
+"use strict";
 
 let score = 0;
 function passQuestion() {
   const questions = [
     {
-      question: 'Is Mohammad Tall person?',
-      yes: { message: 'oh thx brat/sestra', isTrue: true },
-      no: { message: 'how dare you?!', isTrue: false },
-      answer: '',
+      question: "Is Mohammad Tall person?",
+      yes: { message: "oh thx brat/sestra", isTrue: true },
+      no: { message: "how dare you?!", isTrue: false },
+      answer: "",
     },
     {
-      question: 'Is Mohammad a smart person?',
-      yes: { message: 'you have to be smart to realize that', isTrue: true },
-      no: { message: 'how dare you?!', isTrue: false },
-      answer: '',
+      question: "Is Mohammad a smart person?",
+      yes: { message: "you have to be smart to realize that", isTrue: true },
+      no: { message: "how dare you?!", isTrue: false },
+      answer: "",
     },
     {
-      question: 'Is Mohammad addicted to video games?',
-      yes: { message: 'Sadly yes', isTrue: true },
-      no: { message: 'Actually yes he is', isTrue: false },
-      answer: '',
+      question: "Is Mohammad addicted to video games?",
+      yes: { message: "Sadly yes", isTrue: true },
+      no: { message: "Actually yes he is", isTrue: false },
+      answer: "",
     },
     {
-      question: 'Is Mohammad good at programming?',
-      yes: { message: 'Not yet but he is doing his best', isTrue: false },
+      question: "Is Mohammad good at programming?",
+      yes: { message: "Not yet but he is doing his best", isTrue: false },
       no: {
-        message: 'He promised himself to be better and he will',
+        message: "He promised himself to be better and he will",
         isTrue: true,
       },
-      answer: '',
+      answer: "",
     },
     {
-      question: 'Does Mohammad miss Sora?',
-      yes: { message: ':(', isTrue: true },
-      no: { message: 'Very wrong :(', isTrue: false },
-      answer: '',
+      question: "Does Mohammad miss Sora?",
+      yes: { message: ":(", isTrue: true },
+      no: { message: "Very wrong :(", isTrue: false },
+      answer: "",
     },
   ];
   questions.forEach((q) => {
     let { question, yes, no } = q;
-    q.answer = askUser(prompt(question, 'no'), yes, no);
+    q.answer = askUser(prompt(question, "no"), yes, no);
     // if the user answered by yes then he will get a score
     if (q.answer) score++;
   });
@@ -47,42 +47,37 @@ function passQuestion() {
 
 function askForName() {
   while (true) {
-    name = prompt('What is your name', 'user');
-    if (name === null || name === '') continue;
+    name = prompt("What is your name", "user");
+    if (name === null || name === "") continue;
     break;
   }
 
   alert(`Welcome ${name} to my website`);
-  alert('Please answer the following questions by yes or no or y or n');
+  alert("Please answer the following questions by yes or no or y or n");
 }
 
 function askUser(question, onYes, onNo) {
   // added while to make validation
   while (true) {
-    // console.log(question);
-
     if (question === null) {
       question = prompt(
-        'I kindly asked to asnswer by yes or no or y or n 🔪',
-        'yes'
+        "I kindly asked to asnswer by yes or no or y or n 🔪",
+        "yes"
       );
       continue;
     }
     question = question.toLowerCase();
-    // console.log(question);
 
-    if (question === 'yes' || question === 'y') {
+    if (question === "yes" || question === "y") {
       alert(onYes.message);
-      // console.log(onYes.message);
       return onYes.isTrue;
     }
 
-    if (question === 'no' || question === 'n') {
+    if (question === "no" || question === "n") {
       alert(onNo.message);
-      // console.log(onNo.message);
       return onNo.isTrue;
     }
-    question = prompt('I kindly asked to asnswer by yes or no 🔪', 'yes');
+    question = prompt("I kindly asked to asnswer by yes or no 🔪", "yes");
   }
 }
 // gusse the number
@@ -93,7 +88,6 @@ function guessNumber(attmeps = 4, range = 10) {
       `please enter a number between 1 and 10 and you have ${attmeps} trys \n if you clicked cancel or entered empty answer you will lose but at least you will live`
     )
   );
-  // console.log(answer);
 
   for (let i = 1; i < attmeps; i++) {
     if (answer > number || isNaN(answer)) {
@@ -115,23 +109,21 @@ function guessNumber(attmeps = 4, range = 10) {
       );
     }
     if (answer == number) {
-      alert('well done! you are really smart');
+      alert("well done! you are really smart");
       return 1;
     }
   }
-  alert('Game Over');
+  alert("Game Over");
   return 0;
 }
 function maltipleChoice(attmeps = 6) {
   let count = 0;
 
-  const answers = ['katkoota', 'basboosa', 'sora', 'elmoza 78te'];
+  const answers = ["katkoota", "basboosa", "sora", "elmoza 78te"];
 
   let question = prompt(`try to geuss my cat's name and you have ${attmeps}`);
 
   while (true) {
-    // console.log(question);
-
     if (attmeps - count === 1) break;
 
     if (question === null) {
@@ -147,8 +139,6 @@ function maltipleChoice(attmeps = 6) {
         return 1;
       }
     }
-    // console.log(count);
-
     question = prompt(
       `try to geuss my cat's name and you have ${attmeps - count - 1}`
     );
@@ -158,14 +148,11 @@ function maltipleChoice(attmeps = 6) {
   return 0;
 }
 //  ask for name
-let name="";
-
-
-// console.log(questions);
+let name = "";
 askForName();
 passQuestion();
 
 score += guessNumber() + maltipleChoice();
 
-document.querySelector('.user').innerHTML = name;
-document.querySelector('.score').innerHTML = score;
+document.querySelector(".user").innerHTML = name;
+document.querySelector(".score").innerHTML = score;
